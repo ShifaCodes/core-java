a50980ac0a6b50885a77264b2cae3eba38a5506f
Shifa <shifacodes@gmail.com>
2025-09-04 21:03:58 +0530

    Added a new Java file

---
+++ first-idea-program/src/com/shifa/Main.java
@@ -0,0 +1,8 @@
+package com.shifa;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("My Name is Shifa");
+    }
+}
