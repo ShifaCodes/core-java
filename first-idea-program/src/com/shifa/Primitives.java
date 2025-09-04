bca4470cc9792fb52a2e860f139a0abe6ba3b4ae
Shifa <shifacodes@gmail.com>
2025-09-05 00:27:09 +0530

    Some Primitives

---
+++ first-idea-program/src/com/shifa/Primitives.java
@@ -0,0 +1,12 @@
+package com.shifa;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largeDecimalNumbers =3677387.23726;
+        long largeIntegerValues = 56767657898L;
+        boolean check = false;
+    }
+}
