86a81030b25de0a99e67f8edf7552c597f700629
Shifa <shifacodes@gmail.com>
2025-09-20 21:05:43 +0530

    Started loops

---
+++ Conditions-loops/code/src/com/shifa/Loops.java
@@ -0,0 +1,18 @@
+package com.shifa;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+          /*
+            Syntax for loops :
+            for(initialization; condition; increment/decrement) {
+            //body
+            }
+           */
+        // Q: Print numbers from 1 to 5
+        for (int num = 1; num <= 5; num += 1) {
+            System.out.println(num);
+        }
+    }
+}
