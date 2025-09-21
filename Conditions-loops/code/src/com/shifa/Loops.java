9059b324e4a1e0913cb84026976e651d82b02822
Shifa <shifacodes@gmail.com>
2025-09-21 19:58:43 +0530

    Completed some basic loops

--- Conditions-loops/code/src/com/shifa/Loops.java
+++ Conditions-loops/code/src/com/shifa/Loops.java
@@ -11,8 +11,37 @@ public class Loops {
             }
            */
         // Q: Print numbers from 1 to 5
-        for (int num = 1; num <= 5; num += 1) {
-            System.out.println(num);
+//        for (int num = 1; num <= 5; num += 1) {
+//            System.out.println(num);
+//        }
+        // Print numbers from 1 to n
+        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+
+//
+        // While loop
+        /*
+        Syntax :
+            while (condition) {
+            //body
+            }
+         */
+        int num = 1;
+        while (num <= 5) {
+            //    System.out.println(num);
+            num += 1;
         }
+        // Do-While loop
+        /*
+            Syntax:
+            do {
+              //body
+            } while(Condition);
+         */
+        int n = 1;
+        do {
+            System.out.println(n);
+            n++;
+        } while (n <= 5);
     }
 }
