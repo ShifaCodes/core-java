24df9edf774dd67eb11fbcb4732343cf28961db4
Shifa <shifacodes@gmail.com>
2025-09-24 23:21:27 +0530

    Alphabet Case check completed

---
+++ Conditions-loops/code/src/com/shifa/Casecheck.java
@@ -0,0 +1,22 @@
+package com.shifa;
+
+import java.util.Scanner;
+
+public class Casecheck {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        char ch = in.next().trim().charAt(0);
+        
+//        System.out.println(ch);
+
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("lowercase");
+        }else {
+            System.out.println("Uppercase");
+        }
+
+    }
+}
