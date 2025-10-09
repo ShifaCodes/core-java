749baf8c947565c4570a0d8cafb7e3d1b8d0c8d6
Shifa <shifacodes@gmail.com>
2025-10-09 23:48:32 +0530

    Started new lecture-Switch and nested case

---
+++ Switch/src/com/shifa/Main.java
@@ -0,0 +1,32 @@
+package com.shifa;
+
+        import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in  = new Scanner(System.in);
+
+        String fruit = in.next();
+
+        switch (fruit) {
+            case "Mango":
+                System.out.println("King of fruits");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit");
+                break;
+            case "Orange":
+                System.out.println("Round fruit");
+                break;
+            case "Grapes":
+                System.out.println("Small fruit");
+                break;
+            default:
+                System.out.println("Please enter a valid proof");
+
+        }
+        in.close();
+    }
+}
+
