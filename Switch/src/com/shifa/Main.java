0d478ba3d1291c226e2ef3d358b278651145c702
Shifa <shifacodes@gmail.com>
2025-10-10 23:50:51 +0530

    Program-2 using Switch

--- Switch/src/com/shifa/Main.java
+++ Switch/src/com/shifa/Main.java
@@ -5,28 +5,41 @@ package com.shifa;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in  = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-        String fruit = in.next();
+//        String fruit = in.next();
 
-        switch (fruit) {
-            case "Mango":
-                System.out.println("King of fruits");
-                break;
-            case "Apple":
-                System.out.println("A sweet red fruit");
-                break;
-            case "Orange":
-                System.out.println("Round fruit");
-                break;
-            case "Grapes":
-                System.out.println("Small fruit");
-                break;
-            default:
-                System.out.println("Please enter a valid proof");
-
+//        switch (fruit) {
+//            case "Mango":
+//                System.out.println("King of fruits");
+//                break;
+//            case "Apple":
+//                System.out.println("A sweet red fruit");
+//                break;
+//            case "Orange":
+//                System.out.println("Round fruit");
+//                break;
+//            case "Grapes":
+//                System.out.println("Small fruit");
+//                break;
+//            default:
+//                System.out.println("Please enter a valid proof");
+//
+//        }
+//        in.close();
+//    }
+//}
+        int day = in.nextInt();
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
         }
-        in.close();
     }
 }
+
 
