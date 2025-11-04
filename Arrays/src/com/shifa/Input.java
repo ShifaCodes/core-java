0962e10aea91c8e24c86eeb1855b27b9ddc60c45
Shifa <shifacodes@gmail.com>
2025-11-04 22:05:56 +0530

    Arrays of objects and Arrays passing in function

--- Arrays/src/com/shifa/Input.java
+++ Arrays/src/com/shifa/Input.java
@@ -1,11 +1,13 @@
 package com.shifa;
 
 import java.sql.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        //Array of premitives
         int[] arr = new int[5];
         arr[0] = 23;
         arr[1] = 45;
@@ -16,11 +18,11 @@ public class Input {
         System.out.println(arr[3]);
 
         //input using for loops
-       for (int i = 0; i < arr.length; i++) {
-            arr[i] = in.nextInt();
-        }
-
-        System.out.println(Array.toString(arr));
+//       for (int i = 0; i < arr.length; i++) {
+//            arr[i] = in.nextInt();
+//        }
+//
+//        System.out.println(Arrays.toString(arr));
 //
 //        for (int i = 0; i < arr.length; i++) {
 //            System.out.println(arr[i] + " ");;
@@ -30,5 +32,11 @@ public class Input {
 //            System.out.println(num + " "); // here num represents elements of the array
 //        }
         //System.out.println(arr[5]); //index out of bound error
+        //array of objects
+        String[] str = new String[4];
+                for (int i = 0; i < str.length; i++) {
+                  str[i] = in.next();
+                }
+        System.out.println(Arrays.toString(str));
     }
 }
