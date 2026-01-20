7fbdd27df51dfe3a5273b9e2241e05fd44822a14
Shifa <shifacodes@gmail.com>
2026-01-20 21:19:31 +0530

    Question on search in string

--- Linear-search/src/com/shifa/SearchInstrings.java
+++ Linear-search/src/com/shifa/SearchInstrings.java
@@ -6,9 +6,21 @@ public class SearchInstrings {
     public static void main(String[] args) {
         String name = "Shifa";
         char target = 'i';
-        System.out.println(search(name, target));
+     //   System.out.println(search(name, target));
+        System.out.println(Arrays.toString(name.toCharArray()));
 
+    }
+    static boolean search2(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
 
+        for (char ch : str.toCharArray()) {
+         if (ch == target) {
+             return true;
+         }
+        }
+        return false;
     }
 
     static boolean search(String str, char target) {
