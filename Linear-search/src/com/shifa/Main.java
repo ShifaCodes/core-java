76ec31f46362166898eb222140a0987c75283788
Shifa <shifacodes@gmail.com>
2026-01-22 14:25:20 +0530

    search in range and finding min

--- Linear-search/src/com/shifa/Main.java
+++ Linear-search/src/com/shifa/Main.java
@@ -11,6 +11,42 @@ public class Main {
 
     }
 
+    // search the array and return true or false
+
+    static boolean linearSearch3(int[] arr, int target) {
+        if (arr.length ==0) {
+            return false;
+        }
+
+        //run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        //hence the target not found
+        return false;
+    }
+
+    // search the array and return the element
+
+    static int linearSearch2(int[] arr, int target) {
+        if (arr.length ==0) {
+            return -1;
+        }
+
+        //run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return element;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        //hence the target not found
+        return Integer.MAX_VALUE;
+    }
+
     //search in the array: return the index if item found
     //otherwise if them not found return -1
     static int linearSearch(int[] arr, int target) {
