344cc15ddda62f9b4587c599fadff49e51fd9eb0
Shifa <shifacodes@gmail.com>
2026-03-14 16:47:40 +0530

    Binary Search in 2D array completed

--- Binary Search algorithm/src/com/shifa/FirstandLastPosition.java
+++ Binary Search algorithm/src/com/shifa/FirstandLastPosition.java
@@ -4,13 +4,14 @@ public class FirstandLastPosition {
     public static void main(String[] args) {
 
     }
+
     public int[] searchRange(int[] nums, int target) {
 
         int[] ans = {-1, -1};
 
         //check for first occurence if target first
         int start = search(nums, target, true);
-        int end =   search(nums, target, false);
+        int end = search(nums, target, false);
 
         ans[0] = start;
         ans[1] = end;
@@ -19,28 +20,30 @@ public class FirstandLastPosition {
     }
 
     // this function just returns the index value of target
-    int search(int[] nums, int target, boolean findStartIndex){
+    int search(int[] nums, int target, boolean findStartIndex) {
         int ans = -1;
         int start = 0;
         int end = nums.length - 1;
         while (start <= end) {
             // find the middle element
             // int mid = (start + end) /2; // might be possible that (start + end) exceeds the range of int in java
-            int mid = start + (end - start) /2;
+            int mid = start + (end - start) / 2;
 
             if (target < nums[mid]) {
                 end = mid - 1;
-            }else if (target > nums[mid]) {
+            } else if (target > nums[mid]) {
                 start = mid + 1;
             } else {
                 //potential ans found
                 ans = mid;
-                if(findStartIndex) {
-                    end = mid -1;
+                if (findStartIndex) {
+                    end = mid - 1;
                 } else {
-               start = mid + 1;
+                    start = mid + 1;
+                }
             }
+            return ans;
         }
-        return ans;
+        return -1;
     }
-    }
+}
