be56c8914678bd41bbd1670d7509fd52a641241a
Shifa <shifacodes@gmail.com>
2026-04-02 14:20:23 +0530

    Insertion Sort Algorithm - Theory + Code Completed

--- Sorting/src/com/shifa/Main.java
+++ Sorting/src/com/shifa/Main.java
@@ -5,11 +5,24 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {2, -32, 0, 78, 1};
-        selection(arr);
+        int[] arr = {5, 3, 4, 1, 2};
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
 
     }
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    swap(arr, j, j-1);
+                } else {
+                    break;
+                }
+                
+            }
+        }   
+    }
+
     static void selection(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             // find the max item in the remaining array and swap with correct index
