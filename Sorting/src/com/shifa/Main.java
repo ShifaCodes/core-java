f4871ded2353916cf5a93e800751d89ebe442d17
Shifa <shifacodes@gmail.com>
2026-04-02 14:07:19 +0530

    Selection Sort Algorithm - Theory + Code Completed

--- Sorting/src/com/shifa/Main.java
+++ Sorting/src/com/shifa/Main.java
@@ -5,10 +5,35 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {3, 1, 5, 4, 2};
-        bubble(arr);
+        int[] arr = {2, -32, 0, 78, 1};
+        selection(arr);
         System.out.println(Arrays.toString(arr));
 
+    }
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length -i - 1;
+            int maxIndex = getMaxindex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+
+    private static int getMaxindex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
     }
 
     static void bubble(int[] arr) {
@@ -35,3 +60,5 @@ public class Main {
         }
     }
 }
+
+
