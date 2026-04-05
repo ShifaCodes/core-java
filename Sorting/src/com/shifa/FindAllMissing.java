271f7e291614bcc18addbd881a16d7ba54806f07
Shifa <shifacodes@gmail.com>
2026-04-05 13:01:20 +0530

    Cycle Sort- Interview Question Completed

---
+++ Sorting/src/com/shifa/FindAllMissing.java
@@ -0,0 +1,40 @@
+package com.shifa;
+
+import com.sun.tools.javac.code.Attribute;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+
+//https://leetcode.com/problems/find-all-numbers-disapperared-in-a-array/
+//Asked in google
+class FindAllMissing {
+   public List<Integer> findDisapperedNumbers(int[] nums) {
+      int i = 0;
+      while (i < nums.length) {
+         int correct = nums[i] - 1;
+         if (nums[i] != nums[correct]) {
+            swap(nums, i , correct);
+         } else {
+            i++;
+         }
+      }
+      //just find missing number
+      List<Integer> ans = new ArrayList<Integer>();
+      for (int index = 0; index < nums.length; index++) {
+         if (nums[index] != index + 1) {
+       ans.add(index + 1);
+         }
+      }
+      return ans;
+   }
+      static void swap(int[] arr, int first, int second){
+         int temp = arr[first];
+         arr[first] = arr[second];
+         arr[second] = temp;
+      }
+   }
+
+
+
