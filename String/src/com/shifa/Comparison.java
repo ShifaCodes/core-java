5c60f7eca4956b9cd0bdd566f9683848141b7d35
Shifa <shifacodes@gmail.com>
2026-04-07 18:46:41 +0530

    String Comparison

---
+++ String/src/com/shifa/Comparison.java
@@ -0,0 +1,18 @@
+package com.shifa;
+
+public class Comparison {
+    public static void main(String[] args) {
+        String a = "Shifa";
+        String b = "Shifa";
+        String c = a;
+        System.out.println(c == a);
+        //=
+     //   System.out.println(a == b);
+
+        String name1 = new String("Shifa");
+        String name2 = new String("Shifa");
+
+   //     System.out.println(name1 == name2);
+        System.out.println(name1.equals(name2));
+    }
+}
---
+++ String/src/com/shifa/Main.java
@@ -0,0 +1,15 @@
+package com.shifa;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] arr = {2, 3, 5, 4, 19};
+	int num = 10;
+	String name = "Shifa Fatima";
+    //    System.out.println(name);
+        String a = "Shifa";
+        System.out.println(a);
+        a = "Fatima";
+        System.out.println(a);
+    }
+}
