3c53c7a658eaba48c7d478d728d7040e53016b57
Shifa <shifacodes@gmail.com>
2026-04-09 18:14:48 +0530

    Pretty printing in java

--- String/src/com/shifa/Comparison.java
+++ String/src/com/shifa/Comparison.java
@@ -5,14 +5,17 @@ public class Comparison {
         String a = "Shifa";
         String b = "Shifa";
         String c = a;
-        System.out.println(c == a);
+     //   System.out.println(c == a);
         //=
      //   System.out.println(a == b);
 
         String name1 = new String("Shifa");
         String name2 = new String("Shifa");
 
-   //     System.out.println(name1 == name2);
+      System.out.println(name1 == name2);
+
         System.out.println(name1.equals(name2));
+
+       // System.out.println(name1.charAt(0));
     }
 }
---
+++ String/src/com/shifa/PrettyPrinting.java
@@ -0,0 +1,9 @@
+package com.shifa;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1274f;
+        //System.out.printf("Formatted number is %.2f", a);
+        System.out.printf("Pie: %.3f", Math.PI);
+    }
+}
