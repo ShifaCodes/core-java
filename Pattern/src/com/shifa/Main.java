773c228db05c1f0f170255594e74796cd625a751
Shifa <shifacodes@gmail.com>
2026-04-20 14:08:07 +0530

    Pattern question

---
+++ Pattern/src/com/shifa/Main.java
@@ -0,0 +1,64 @@
+package com.shifa;
+
+public class Main {
+    public static void main(String[] args) {
+       pattern5(4);
+    }
+    static void pattern5(int n) {
+        for (int row = 0; row < 2 * n; row++) {
+            int totalColInRow = row > n ? 2 * n - row: row;
+            for (int col = 0; col < totalColInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+    static void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+            //for every row, run the col
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+
+            }
+            //when one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n) {
+        for (int row = 1; row <= n; row++) {
+            //for every row, run the col
+            for (int col = 1; col <= n-row+1; col++) {
+                System.out.print("* ");
+
+            }
+            //when one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+
+    static void pattern1(int n) {
+        for (int row = 1; row <= n; row++) {
+            //for every row, run the col
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+
+            }
+            //when one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int row = 1; row <= n; row++) {
+            //for every row, run the col
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+
+            }
+            //when one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+}
