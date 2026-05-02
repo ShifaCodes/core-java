7d0b4233dc1fff4db30ef90af3f508f08211ddfc
Shifa <shifacodes@gmail.com>
2026-05-02 22:25:07 +0530

    More Pattern questions

--- Pattern/src/com/shifa/Main.java
+++ Pattern/src/com/shifa/Main.java
@@ -2,7 +2,65 @@ package com.shifa;
 
 public class Main {
     public static void main(String[] args) {
-       pattern5(4);
+
+    }
+
+    static void pattern31(int n) {
+        for (int row = 0; row < 2 * n - 1; row++) {
+            for (int col = 0; col < 2 * n - 1; col++) {
+                int atEveryIndex = 4567;
+                System.out.print();
+            }
+            System.out.println();
+        }
+    }
+    static void pattern30(int n) {
+        for (int row = 1; row <= n; row++) {
+
+            for (int space = 0; space < n-row; space++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern17(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+
+          int c = row > n ? 2 * n - row: row;
+
+            for (int space = 0; space < n-c; space++) {
+                System.out.print("  ");
+            }
+            for (int col = c; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    static void pattern28(int n) {
+        for (int row = 0; row < 2 * n; row++) {
+            int totalColInRow = row > n ? 2 * n - row: row;
+
+            int noOfSpaces = n - totalColInRow;
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < totalColInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
     static void pattern5(int n) {
         for (int row = 0; row < 2 * n; row++) {
