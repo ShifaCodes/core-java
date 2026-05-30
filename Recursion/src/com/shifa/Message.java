5205bb4b61a996b4aeb5a985297a7bb9abef76ac
Shifa <shifacodes@gmail.com>
2026-05-30 18:54:13 +0530

    Code for Finding fibonacci No using Recursion

---
+++ Recursion/src/com/shifa/Message.java
@@ -0,0 +1,34 @@
+package com.shifa;
+
+public class Message {
+
+    public static void main(String[] args) {
+        // write a function that prints hello world 5 times and call it in single function
+        message();
+    }
+
+    static void message() {
+        System.out.println("Hello World");
+        message1();
+    }
+
+    static void message1() {
+        System.out.println("Hello World");
+        message2();
+    }
+
+    static void message2() {
+        System.out.println("Hello World");
+        message3();
+    }
+
+    static void message3() {
+        System.out.println("Hello World");
+        message4();
+    }
+
+    static void message4() {
+        System.out.println("Hello World");
+    }
+}
+
---
+++ Recursion/src/com/shifa/fibo.java
@@ -0,0 +1,17 @@
+package com.shifa;
+
+public class fibo {
+    public static void main(String[] args) {
+        int ans = fibo(4);
+        System.out.println(ans);
+    }
+    // as the function call is returning an integer
+    static int fibo(int n) {
+        //base condition
+        if (n < 2) {
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+
+}
