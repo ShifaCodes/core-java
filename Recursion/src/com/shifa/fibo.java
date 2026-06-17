c4345eed6c0175dece116b42189b1eff949eb295
Shifa <shifacodes@gmail.com>
2026-06-17 22:06:38 +0530

    Time and Space Complexity tutorial Completed

--- Recursion/src/com/shifa/fibo.java
+++ Recursion/src/com/shifa/fibo.java
@@ -2,8 +2,13 @@ package com.shifa;
 
 public class fibo {
     public static void main(String[] args) {
-        int ans = fibo(4);
-        System.out.println(ans);
+    //    for (int i = 0; i < 11; i++) {
+      //      System.out.println(fiboFormula(i));
+    //}
+        System.out.println(fiboFormula(50));
+    }
+    static int fiboFormula(int n) {
+        return (int)(Math.pow(((1 + Math.sqrt(5)) / 2), n) - (Math.pow(((1 - Math.sqrt(5)) / 2), n)) / Math.sqrt(5));
     }
     // as the function call is returning an integer
     static int fibo(int n) {
